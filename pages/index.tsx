f1cc4e28133cf6a09a8b36caefa790565cebfebd
lAbYriNth <rxnayeemimi@gmail.com>
2022-04-22 15:04:02 +0600

    style updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import EditableEl from "../components/EditableEl";
 
 function TableRow() {
     return (
-        <div className="js__row flex w-auto justify-center gap-4 border-b py-2 last:border-b-0">
+        <div className="js__row mt-20 flex w-auto justify-center gap-4 border-b py-2 last:border-b-0">
             <EditableEl pnl={76} />
         </div>
     );
